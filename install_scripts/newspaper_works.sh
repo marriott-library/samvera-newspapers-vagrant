fe3ab1cc54a00eb5ab3f25419fb800b73b5248fe
Brian McBride <brian.mcbride@utah.edu>
2018-08-07 19:37:52 -0600

    updated newspaper provisioning

--- install_scripts/newspaper_works.sh
+++ install_scripts/newspaper_works.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 HOME="/home/ubuntu"
 
-
 echo "Creating Newspaper Works demo in ${HOME}/newspaper_works"
-cd
-git clone https://github.com/marriott-library/newspaper_works.git newspaper_works
+cd ${HOME}
+sudo -u ubuntu git clone https://github.com/marriott-library/newspaper_works.git
 cd newspaper_works
-#/bundle install --quiet --path vendor/bundle
-#rake engine_cart:generate
+sudo -u ubuntu bundle install --quiet --path vendor/bundle
+sudo -u ubuntu rake engine_cart:generate
 echo "Installation of Newspaper Works App complete..."
