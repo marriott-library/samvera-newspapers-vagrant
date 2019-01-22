7426265f823e3e048ad91377751c7719a1d02009
Brian McBride <brian.mcbride@utah.edu>
2019-01-22 15:15:50 -0700

    Updated username

--- install_scripts/newspaper_works.sh
+++ install_scripts/newspaper_works.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-HOME="/home/ubuntu"
+HOME="/home/vagrant"
 
 echo "Creating Newspaper Works demo in ${HOME}/newspaper_works"
 cd ${HOME}
-sudo -u ubuntu git clone https://github.com/marriott-library/newspaper_works.git newspaper_works
+sudo -u vagrant git clone https://github.com/marriott-library/newspaper_works.git newspaper_works
 cd newspaper_works
-sudo -u ubuntu bundle install # --quiet --path vendor/bundle
-sudo -u ubuntu rake engine_cart:generate
+sudo -u vagrant bundle install # --quiet --path vendor/bundle
+sudo -u vagrant rake engine_cart:generate
 echo "Installation of Newspaper_Works is complete..."
