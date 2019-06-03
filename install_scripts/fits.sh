e0b5cefb31004f16a553b7833719fba56a838cd1
Brian McBride <brian.mcbride@utah.edu>
2019-06-03 10:27:42 -0600

    Updated download location for FITS

--- install_scripts/fits.sh
+++ install_scripts/fits.sh
@@ -8,7 +8,7 @@ if [ -f "$SHARED_DIR/install_scripts/config" ]; then
 fi
 
 if [ ! -d fits-$FITS_VERSION ]; then
-  DOWNLOAD_URL="https://brussels.lib.utah.edu/FITS/fits-${FITS_VERSION}.zip"
+  DOWNLOAD_URL="https://github.com/harvard-lts/fits/archive/${FITS_VERSION}.zip"
   cd $DOWNLOAD_DIR
   if [ ! -f "fits.zip" ]; then
     curl $DOWNLOAD_URL -o fits.zip
