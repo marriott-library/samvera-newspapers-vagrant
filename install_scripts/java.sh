9a1d8dd9c46bd0a9bed255beb452482ba4a31f9c
Sean Upton <sdupton@gmail.com>
2019-09-26 19:46:57 -0600

    Provisioning scripts make OpenJDK 8 the default Java version.

--- install_scripts/java.sh
+++ install_scripts/java.sh
@@ -4,8 +4,12 @@
 
 # Java
 if which java >/dev/null; then
-  echo "skip java 9 installation"
+  echo "skip java installation"
 else
-  echo "java 9 installation"
+  # install distro default
+  echo "java installation"
   apt-get install --yes default-jdk
+  # ...but make JDK 8 default java executable (for Fedora, Solr, etc)
+  apt-get install openjdk-8-jre-headless
+  update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 fi
