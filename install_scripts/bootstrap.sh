596367ed5de520915b19511423429ecb3c458ea7
Brian McBride <brian.mcbride@utah.edu>
2020-07-06 13:07:47 -0600

    added libgbm1 to the installation script

--- install_scripts/bootstrap.sh
+++ install_scripts/bootstrap.sh
@@ -26,7 +26,7 @@ apt-get -y install git vim
 apt-get -y install wget curl unzip htop
 
 # Install Google Chrome
-sudo apt-get install -y libappindicator3-1 fonts-liberation libasound2 libnspr4 libnss3 libxss1 xdg-utils libatk-bridge2.0 libgtk-3-0
+sudo apt-get install -y libappindicator3-1 fonts-liberation libasound2 libnspr4 libnss3 libxss1 xdg-utils libatk-bridge2.0 libgtk-3-0 libgbm1
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 #sudo apt-get install fonts-liberation libxss1 libgconf2-4 libappindicator1 libindicator7 libappindicator3-1 libatk-bridge2.0-0 libatk1.0-0 libatspi2.0-0 libcairo2 libcups2 libgdk-pixbuf2
